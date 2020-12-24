b16e03e1b940b7e72d4513c9e63eb34a82440cf1
Berliana Farah <42767850+berlianafd@users.noreply.github.com>
2020-12-24 19:34:41 +0700

    initial commit

---
+++ Assets/Script/menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+     public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+    public void GoToMainMenu()
+    {
+        Application.LoadLevel("main_menu");
+    }
+    public void GoToARCamera()
+    {
+        Application.LoadLevel("ARCameraMain");
+    }
+    public void GoToGuideScene()
+    {
+        Application.LoadLevel("scene_guide");
+    }
+    public void GoToJaguarScene()
+    {
+        Application.LoadLevel("scene_jaguar");
+    }
+    public void exitaplikasi()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/splash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class splash : MonoBehaviour
+{
+    void Start() {
+		StartCoroutine(Example());
+	}
+
+	IEnumerator Example() {
+		yield return new WaitForSeconds(24);
+		Application.LoadLevel ("loading");
+	}
+
+}
